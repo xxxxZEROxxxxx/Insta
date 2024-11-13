a5c91475b961f43efc9cb5eceda0f8d661784ecc
alaa eddin <sjamal.2020@gmail.com>
2024-11-13 13:16:47 +0200

    update

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,8 +1,24 @@
+import { Outlet, Navigate } from 'react-router-dom';
 
 const AuthLayout = () => {
-  return (
-    <div>AuthLayout</div>
-  )
-}
+  const isAuth = false;
 
-export default AuthLayout
+  if (isAuth) {
+    return <Navigate to="/" />;
+  }
+
+  return (<>
+    <section className="flex flex-1 justify-center items-center flex-col py-10">
+      <Outlet />
+   
+    </section>
+    <img 
+        src="/assets/images/side-img.svg" 
+        alt="logo" 
+        className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat" 
+      />
+    </>
+  );
+};
+
+export default AuthLayout;
