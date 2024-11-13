37e25f0050a490b55996e64b3610664a4f3ada62
alaa eddin <115365686+xxxxZEROxxxxx@users.noreply.github.com>
2024-11-13 18:59:07 +0300

    update

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -1,12 +1,67 @@
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+
 import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { SiginValidation } from "@/lib/validation"
+
+
+
 
 
 
 const SigninForm = () => {
+
+  const form = useForm<z.infer<typeof SiginValidation>>({
+    resolver: zodResolver(SiginValidation),
+    defaultValues: {
+      name:"",
+      username: "",
+      email:"",
+      password:"",
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof SiginValidation>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
   return (
-    <div>
-       <Button>Click me</Button>
-    </div>
+   
+       <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+   
   )
 }
 
---
+++ src/lib/validation/index.ts
@@ -0,0 +1,7 @@
+import { z } from "zod"
+export const SiginValidation = z.object({
+    name:z.string().min(2,{message:'Too short'}),
+    username: z.string().min(2,{message:'Too short'}),
+    email:z.string().email(),
+    password:z.string().min(8,{message:'Password Must be at lest 8 characters'}),
+  })
