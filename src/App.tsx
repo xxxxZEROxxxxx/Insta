697fe2775509fda185ad8f1cedfb192a52b036e9
alaa eddin <sjamal.2020@gmail.com>
2024-11-13 12:38:56 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import AuthLayout from './_auth/AuthLayout';
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+import Home from './_root/pages/Home';
+import RootLayout from './_root/RootLayout';
+import './globels.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Router>
+      <main className="flex max-h-screen">
+        <Routes>
+          <Route element={<AuthLayout/>}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+          </Route>
+      <Route element={<RootLayout/>}>
+      <Route index element={<Home />} />
 
-export default App
+      </Route>
+      
+        
+        </Routes>
+      </main>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/_auth/forms/SigninForm.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+
+
+const SigninForm = () => {
+  return (
+    <div>
+       <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default SigninForm
