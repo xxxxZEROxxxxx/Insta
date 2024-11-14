c520a9de8fff2c1fabe0c75e8c7c45aca362cd12
alaa eddin <sjamal.2020@gmail.com>
2024-11-14 12:01:10 +0200

    npm install appwrite@14.0.1

---
+++ src/lib/appWrite/api.ts
@@ -0,0 +1,21 @@
+
+import {INewUser} from "@/types"
+import { ID } from "appwrite";
+import { account } from "./confg";
+
+
+
+export async function createUserAccount(user: INewUser) {
+try{
+    const newAccount =await account.create(
+        ID.unique(),
+        user.email,
+        user.password,
+        user.name
+    )
+
+} catch (error){
+    console.log(error);
+    return(error)
+}
+}
